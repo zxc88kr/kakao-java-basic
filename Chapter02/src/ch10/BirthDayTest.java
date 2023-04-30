2a4d23e1981ba5e29085eea4c1beb23566e5a022
zxc88kr <zxc88kr@naver.com>
2023-05-01 04:27:30 +0900

    Chapter02 ch10

---
+++ Chapter02/src/ch10/BirthDayTest.java
@@ -0,0 +1,13 @@
+package ch10;
+
+public class BirthDayTest {
+    public static void main(String[] args) {
+        BirthDay date = new BirthDay();
+
+        date.setYear(2019);
+        date.setMonth(13);
+        date.setDay(30);
+
+        date.showDate();
+    }
+}
