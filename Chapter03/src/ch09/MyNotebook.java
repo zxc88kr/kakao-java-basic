63cc53f3a41df284b98d5e315ddaa00f912b92a7
zxc88kr <zxc88kr@naver.com>
2023-05-01 14:21:55 +0900

    Chapter03 ch09

---
+++ Chapter03/src/ch09/MyNotebook.java
@@ -0,0 +1,8 @@
+package ch09;
+
+public class MyNotebook extends Notebook {
+    @Override
+    public void typing() {
+        System.out.println("Notebook typing");
+    }
+}
---
+++ Chapter03/src/ch09/Notebook.java
@@ -0,0 +1,8 @@
+package ch09;
+
+public abstract class Notebook extends Computer {
+    @Override
+    public void display() {
+        System.out.println("Notebook display");
+    }
+}
