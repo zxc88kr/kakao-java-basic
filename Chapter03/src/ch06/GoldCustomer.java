5bf9b9b88250603f3c8b4586f44f2360210693fa
zxc88kr <zxc88kr@naver.com>
2023-05-01 13:31:59 +0900

    Chapter03 ch06

---
+++ Chapter03/src/ch06/GoldCustomer.java
@@ -0,0 +1,20 @@
+package ch06;
+
+public class GoldCustomer extends Customer {
+    double salesRatio;
+
+    public GoldCustomer(int customerId, String customerName) {
+        super(customerId, customerName);
+
+        customerGrade = "GOLD";
+        bonusRatio = 0.02;
+        salesRatio = 0.1;
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        price -= (int)(price * salesRatio);
+        return price;
+    }
+}
