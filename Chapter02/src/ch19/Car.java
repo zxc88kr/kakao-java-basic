30275d19183b9f2b0bba1a524fb8bfd6ce2acfa6
zxc88kr <zxc88kr@naver.com>
2023-05-01 11:26:27 +0900

    Chapter02 ch19

---
+++ Chapter02/src/ch19/Car.java
@@ -0,0 +1,16 @@
+package ch19;
+
+public class Car {
+    private static int serialNum = 10000;
+
+    private int carNum;
+
+    public Car() {
+        serialNum++;
+        carNum = serialNum;
+    }
+
+    public int getCarNum() {
+        return carNum;
+    }
+}
