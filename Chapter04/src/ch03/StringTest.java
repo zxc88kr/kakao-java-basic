0368f0cbdb2a767d1aea5d69bc2b620823c4d458
zxc88kr <zxc88kr@naver.com>
2023-05-10 00:39:57 +0900

    Chapter04 ch03

---
+++ Chapter04/src/ch03/StringTest.java
@@ -0,0 +1,23 @@
+package ch03;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String java = new String("java");
+        String android = new String("android");
+        /*
+        System.out.println(System.identityHashCode(java));
+        java = java.concat(android);
+        System.out.println(System.identityHashCode(java));
+
+        System.out.println(java);
+        */
+        StringBuilder buffer = new StringBuilder(java);
+
+        System.out.println(System.identityHashCode(buffer));
+        buffer.append(android);
+        System.out.println(System.identityHashCode(buffer));
+
+        String test = buffer.toString();
+        System.out.println(test);
+    }
+}
